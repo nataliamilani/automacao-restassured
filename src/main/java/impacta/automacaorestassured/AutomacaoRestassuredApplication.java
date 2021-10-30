22060a4e16c65b2b37c2cf75d33aab5806f3e811
Natalia Milani <natalia.milani@hotmail.com>
2021-10-29 21:35:37 -0300

    subida projeto

---
+++ src/main/java/impacta/automacaorestassured/AutomacaoRestassuredApplication.java
@@ -0,0 +1,13 @@
+package impacta.automacaorestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaoRestassuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaoRestassuredApplication.class, args);
+	}
+
+}
